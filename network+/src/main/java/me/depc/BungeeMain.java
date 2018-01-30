c55cc1d7c94d30d4f04e887a7aed783ab84ce6a1
mukruka <mukruka@gmail.com>
2018-01-30 12:38:46 -0700

    initial commit for bungee plugin

---
+++ network+/src/main/java/me/depc/BungeeMain.java
@@ -0,0 +1,10 @@
+package me.depc;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class BungeeMain extends Plugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Network+ initialized.");
+    }
+}
